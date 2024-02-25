3ac74b426a74cc0c84ddac103592951ae4564291
hhovey <89988053+hhovey@users.noreply.github.com>
2024-02-25 17:35:46 -0500

    Update nmap_scanner.py

--- nmap_scanner.py
+++ nmap_scanner.py
@@ -1,8 +1,8 @@
 import nmap
 
 nm = nmap.PortScanner()
-
-target = "45.36.31.154"
+#The target is a random IP number.
+target = "42.31.38.124"
 options = "-sV -sC scan_results"
 
 nm.scan(target, arguments=options)
@@ -12,6 +12,6 @@ for host in nm.all_hosts():
     print("State: %s" % nm[host].state())
     for protocol in nm[host].all_protocols(): 
         print("Protocol: %s" % protocol)
-        port_info = nm[host][protocol]
-        for port, state in port_info.items():
+        port_i = nm[host][protocol]
+        for port, state in port_i.items():
             print("Print: %s\tState: %s" % (port, state))
